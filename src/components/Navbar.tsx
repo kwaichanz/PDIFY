261daaae00ec1a95efed26096b32908d094ab861
kwaichanz <alphezelphaz@gmail.com>
2024-01-14 23:08:39 +0700

    Update upload file and redirect after

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -42,7 +42,7 @@ const Navbar: FC<NavbarProps> = () => {
                   size: "sm",
                 })}
               >
-                Get startged <ArrowRight className="ml-1.5 h-5 w-5" />
+                Get started <ArrowRight className="ml-1.5 h-5 w-5" />
               </RegisterLink>
             </>
           </div>
--- src/components/UploadButton.tsx
+++ src/components/UploadButton.tsx
@@ -5,16 +5,29 @@ import { Dialog, DialogContent, DialogTrigger } from "./ui/dialog";
 import { Button } from "./ui/button";
 
 import Dropzone from "react-dropzone";
-import { Cloud, File } from "lucide-react";
+import { Cloud, File, Loader2 } from "lucide-react";
 import { Progress } from "./ui/progress";
 import { useUploadThing } from "@/app/lib/uploadthing";
 import { useToast } from "./ui/use-toast";
+import { trpc } from "@/app/_trpc/client";
+import { useRouter } from "next/navigation";
 
 const UploadDropzone = () => {
-  const [isUploading, setIsUploading] = useState<boolean | null>(true);
+  const router = useRouter();
+
+  const [isUploading, setIsUploading] = useState<boolean | null>(false);
   const [uploadProgress, setUploadProgress] = useState<number>(0);
 
   const { startUpload } = useUploadThing("pdfUploader");
+
+  const { mutate: startPolling } = trpc.getFile.useMutation({
+    onSuccess: (file) => {
+      router.push(`/dashboard/${file.id}`);
+    },
+    retry: true,
+    retryDelay: 500,
+  });
+
   const { toast } = useToast();
 
   const startSimulatedProgress = () => {
@@ -64,10 +77,12 @@ const UploadDropzone = () => {
           });
         }
 
-        await new Promise((resolve) => setTimeout(resolve, 100000));
+        // await new Promise((resolve) => setTimeout(resolve, 100000));
 
         clearInterval(progressInterval);
         setUploadProgress(100);
+
+        startPolling({ key });
       }}
     >
       {({ getRootProps, getInputProps, acceptedFiles }) => (
@@ -105,9 +120,25 @@ const UploadDropzone = () => {
                   <Progress
                     value={uploadProgress}
                     className="h-1 w-full bg-zinc-200"
+                    indicatorColor={
+                      uploadProgress === 100 ? "bg-green-500" : ""
+                    }
                   />{" "}
+                  {uploadProgress === 100 ? (
+                    <div className="flex gap-1 items-center justify-center text-sm text-zinc-500 text-center pt-2">
+                      <Loader2 className="h-3 w-3 animate-spin" />
+                      Redirecting...
+                    </div>
+                  ) : null}
                 </div>
               ) : null}
+
+              <input
+                {...getInputProps()}
+                type="file"
+                id="dropzone-file"
+                className="hidden"
+              />
             </label>
           </div>
         </div>
