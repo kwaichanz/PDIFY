83982ebb6e569a94acd9a88aabeb9ac9f36f7057
kwaichanz <alphezelphaz@gmail.com>
2024-01-21 19:10:27 +0700

    Update PDFRenderer component with PDF display, pages, zoom, rotation

--- src/app/dashboard/[fileid]/page.tsx
+++ src/app/dashboard/[fileid]/page.tsx
@@ -32,8 +32,8 @@ const Page = async ({ params }: PageProps) => {
       <div className="mx-auto w-full max-w-8xl grow lg:flex xl:px-2">
         {/* left side */}
         <div className="flex-1 xl:flex">
-          <div className="px-4 py-6 sm:px-6 lg:pl-8 xl:flex-1 xl:pl-6">
-            <PdfRenderer />
+          <div className="px-4 py-6 sm:px-6 lg:psl-8 xl:flex-1 xl:pl-6">
+            <PdfRenderer url={file.url}/>
           </div>
         </div>
 
